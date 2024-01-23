b0f2502e4f37774ee508db4c532163793cad428d
Anam Aadel <rruholislam1998@gmail.com>
2024-01-23 23:35:11 +0600

    updated what people say section

--- src/components/homeComponents/WhatPeopleSay.js
+++ src/components/homeComponents/WhatPeopleSay.js
@@ -3,12 +3,13 @@ import Marquee from "react-fast-marquee";
 async function WhatPeopleSay() {
     
     const res = await fetch("https://online-surbey.vercel.app/testimonials.json");
+
     const data = await res.json();
 
     
     return (
         <section className="py-6 bg-slate-100 text-gray-800">
-            <div className="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
+            <div className="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10" >
                 <h1 className="text-4xl font-bold leadi text-center sm:text-5xl">What people Say About Our Service</h1>
                 <Marquee pauseOnHover={true}>
                     <div className="flex mt-8">
