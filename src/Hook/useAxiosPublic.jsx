dd63012d8dd38c62450c37893c2c4c277782794f
sadat khan <sadatcse@gmail.com>
2024-01-24 10:24:39 +0600

    add login page

--- src/Hook/useAxiosPublic.jsx
+++ src/Hook/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://universal-survey-backend.vercel.app/'
 })
 
 const useAxiosPublic = () => {
