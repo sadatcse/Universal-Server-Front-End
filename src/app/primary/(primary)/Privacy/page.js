6633bade172d72394931f71dbfc3c9076f924351
Md-Mansor <mansor.me777@gmail.com>
2024-01-31 17:07:45 +0600

    privacy and copy right page

---
+++ src/app/primary/(primary)/Privacy/page.js
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+
+const Privacy = () => {
+    return (
+        <div>
+            <pre>
+                Thank you for using Universal Survey! This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our online survey platform Service.
+
+                1. Information We Collect:
+
+                1.1 Personal Information:
+                When you register for an account, we may collect personal information such as your name, email address, and any other information you provide voluntarily.
+
+                1.2 Survey Data:
+                We collect and store the survey data you create, including questions, responses, and any other information entered by you and your respondents.
+
+                1.3 Usage Information:
+                We may automatically collect information about your use of the Service, including log files, IP addresses, browser type, and pages visited.
+
+                2. How We Use Your Information:
+
+                2.1 Service Delivery:
+
+                We use your personal information to provide, maintain, and improve our Service, ensuring a personalized and efficient user experience.
+                2.2 Communication:
+
+                We may use your email address to send you important updates, newsletters, or information related to our Service. You can opt-out of these communications at any time.
+                2.3 Analytics:
+
+                We use analytics tools to understand how users interact with our platform, helping us improve and optimize our services.
+
+                3. Sharing Your Information:
+                3.1 Survey Data:
+
+                Survey data is confidential and will not be shared with third parties unless required by law.
+                3.2 Service Providers:
+
+                We may engage third-party service providers to assist with the operation of the Service, and they may have access to your information.
+                4. Security:
+
+                4.1 Data Security:
+
+                We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or destruction.
+                5. Your Choices:
+
+                5.1 Account Information:
+
+                You can review and update your account information through your profile settings.
+                5.2 Communication Preferences:
+
+                You can opt-out of receiving promotional emails by following the instructions provided in the email.
+                6. Changes to This Privacy Policy:
+
+                6.1 Updates:
+
+                We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+                7. Contact Us:
+
+                7.1 Questions:
+
+                If you have any questions about this Privacy Policy, please contact us at.
+                By using our Service, you agree to the terms outlined in this Privacy Policy.
+            </pre>
+        </div>
+    );
+};
+
+export default Privacy;
