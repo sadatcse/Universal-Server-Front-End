d4ddde7674dcbb11a0ad27288961976e6a3d4efb
sadat khan <sadatcse@gmail.com>
2024-02-01 10:22:13 +0600

    Update password page

--- src/app/dashboard/(dashboard)/change_password/page.js
+++ src/app/dashboard/(dashboard)/change_password/page.js
@@ -1,7 +1,77 @@
+"use client"
+import { useState } from "react";
 
 function page() {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+//Add Firebase code 
+    console.log('Password change logic goes here');
+  };
   return (
-    <div>page</div>
+    <div className="min-h-screen flex items-center justify-center">
+    <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Change Password</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="currentPassword" className="block text-gray-700 text-sm font-bold mb-2">
+            Current Password
+          </label>
+          <input
+            type="password"
+            id="currentPassword"
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="newPassword" className="block text-gray-700 text-sm font-bold mb-2">
+            New Password
+          </label>
+          <input
+            type="password"
+            id="newPassword"
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+          />
+        </div>
+
+   
+        <div className="mb-6">
+          <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+
+    
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none"
+        >
+          Change Password
+        </button>
+      </form>
+    </div>
+  </div>
   )
 }
 
