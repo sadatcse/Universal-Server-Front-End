5da5e933fc1f3cc23614e94022cc188a4af371b5
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-03 10:28:31 +0600

    final changes

--- src/app/dashboard/(dashboard)/admin/new_user/page.jsx
+++ src/app/dashboard/(dashboard)/admin/new_user/page.jsx
@@ -1,5 +1,4 @@
 'use client'
-import { data } from 'autoprefixer';
 import React, { useEffect, useState } from 'react'
 import { CiSearch } from "react-icons/ci";
 import { HiOutlineEye } from "react-icons/hi";
@@ -7,12 +6,21 @@ import { RiDeleteBinLine } from "react-icons/ri";
 
 
 function NewUsers() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('item2');
   const [userData, setUserData] = useState([]);
+  // fetching the user data from db
   useEffect(() => {
     fetch('https://universal-survey-backend.vercel.app/users')
       .then(res => res.json())
       .then(data => setUserData(data))
-  }, [])
+  }, []);
+
+  // dropdown
+  const handleChange = (e) => {
+    setShowDropdown(!showDropdown);
+    setSelectedValue(e.target.value);
+  };
   return (
     <section className='md:w-[95%] mx-auto'>
       <h1 className='text-xl font-bold'>User List</h1>
@@ -26,14 +34,28 @@ function NewUsers() {
       </div>
       {/* main div */}
       <div className='mt-4 p-4 rounded-lg border border-gray-100 shadow-sm'>
-        {/* search div */}
-        <div className='flex p-2 rounded-lg shadow-sm border gap-2'>
-          <button className='text-2xl ml-1'><CiSearch /></button>
-          <input placeholder='Search by name or email' className='text-sm outline-none w-full'></input>
+        {/* search and dropdown div */}
+        <div className='flex justify-between gap-3 '>
+          <div className='flex p-2 rounded-lg shadow-sm w-3/12 border gap-2 relative'>
+            <div className='text-sm outline-none w-full' value={selectedValue} onClick={()=>{setShowDropdown(!showDropdown)}} onChange={handleChange}>
+              
+            </div>
+            <ul onClick={()=>{setShowDropdown(!showDropdown)}} className={`menu absolute animate__animated animate__fadeIn  bg-white border shadow w-full top-12 z-20 left-0 rounded-lg ${showDropdown ? 'block' : 'hidden'}`}>
+              <li><a>Item 1</a></li>
+              <li><a>Item 2</a></li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </div>
+          <div className='flex p-2 rounded-lg shadow-sm w-9/12 border gap-2 '>
+            <button className='text-2xl ml-1'><CiSearch /></button>
+            <input placeholder='Search by name or email' className='text-sm outline-none w-full'></input>
+          </div>
         </div>
         {/* total user */}
         <div className='mt-4 text-gray-400 text-sm'>
-          <h1>Total {userData.length} user</h1>
+          <h1>{userData.length === 0 ? <>
+            <div className="skeleton h-4 w-20 mt-5 opacity-50"></div>
+          </> : <>Total {userData.length} user</>}</h1>
         </div>
         {userData.length == 0 ?
           <>
