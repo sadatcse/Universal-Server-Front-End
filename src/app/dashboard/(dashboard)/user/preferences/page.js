e539550c2660916de3f17ccd11a82df122ce0664
Md-Mansor <mansor.me777@gmail.com>
2024-02-06 17:53:39 +0600

    prefrances page front end design

--- src/app/dashboard/(dashboard)/user/preferences/page.js
+++ src/app/dashboard/(dashboard)/user/preferences/page.js
@@ -1,6 +1,39 @@
 
 export default function Page() {
   return (
-    <div>page</div>
+    <div>
+      <div>
+        <div className="min-h-screen bg-amber-200 border rounded-md ">
+          <form className="p-10">
+            <h1 className="text-end font-normal text-xl"><button className=" btn btn-accent">Edit</button></h1>
+            <h1 className="text-end font-normal text-xl"><button className=" btn btn-accent">Save</button></h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Time Zone</span>
+              </label>
+              <input type="time" placeholder="time" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date/Time Format</span>
+              </label>
+              <input type="date" placeholder="date/time" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Language</span>
+              </label>
+              <select name="language" id="">
+                <option value="English">English</option>
+                <option value="Bangla">Bangla</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Spanish">Spanish</option>
+              </select>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
