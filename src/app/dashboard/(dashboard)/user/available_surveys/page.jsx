a6cefd86bbdc595c8bf45157fb1dff6d82e4c456
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-12 15:10:12 +0600

    completed the full available surveys page's ui

--- src/app/dashboard/(dashboard)/user/available_surveys/page.jsx
+++ src/app/dashboard/(dashboard)/user/available_surveys/page.jsx
@@ -22,13 +22,24 @@ export default function Page() {
   }, []);
 
   // Single survey card
-  const SurveyCard = () => {
+  const SurveyCard = ({ survey }) => {
+    const { title, description, creator, deadline } = survey;
     return (
-      <div>
-        <p>i am a survey</p>
-      </div>
-    )
-  }
+        <div className=" rounded-lg shadow border p-6 mb-4">
+            <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
+            <p className="text-sm text-gray-600 mb-4">{description}</p>
+            <div className="flex items-center justify-between">
+                <p className="text-sm text-gray-600">Creator: {creator}</p>
+                <p className="text-sm text-gray-600">Deadline: {deadline}</p>
+            </div>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                Take Survey
+            </button>
+        </div>
+    );
+};
+
+
 
   return (
 
@@ -37,22 +48,22 @@ export default function Page() {
       {/* Head */}
       <header>
         <div class="p-8 md:p-10 rounded-lg mb-5 shadow-lg bg-gradient-to-r from-blue-300 to-blue-500 text-center text-white">
-          <h1 class="font-semibold text-5xl mb-3">
+          <h1 class="font-semibold  text-4xl md:text-[40px] mb-3">
             Your Opinion Matters
           </h1>
-          <h4 class="text-lg font-normal">
+          <h4 class="font-normal">
             Help Us Improve by Sharing Your Feedback
           </h4>
         </div>
       </header>
 
       {/* Survey cards */}
-      <main>
+      <main className="grid  grid-cols-1 lg:grid-cols-2 gap-3">
         {
           surveys ?
             <>
               {/* While surveys data fatched */}
-              {surveys.map(survey => <p>hi</p>)}
+              {surveys.map(survey => <SurveyCard survey={survey} key={survey.title} />)}
             </> : <>
               {/* Skeletons while surveys are loading */}
               <p>no</p>
