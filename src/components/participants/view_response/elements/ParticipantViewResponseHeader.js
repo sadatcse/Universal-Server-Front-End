4a02ba234464848c26928e79555fbf2635ac51a6
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-14 12:07:03 +0600

    fixed a bug

--- src/components/participants/view_response/elements/ParticipantViewResponseHeader.js
+++ src/components/participants/view_response/elements/ParticipantViewResponseHeader.js
@@ -3,14 +3,13 @@ import ParticipantViewResponseSearch from './ParticipantViewResponseSearch'
 
 function ParticipantViewResponseHeader() {
   return (
-    <div className='my-4 mb-5  w-[97%] mx-auto relative flex justify-between items-center'>
+    <div className='my-4 mb-5   mx-auto relative flex justify-between items-center'>
       <div className='flex flex-col '>
         <h2 className="text-4xl font-semibold  flex items-center gap-3">
           View Response</h2>
         <p className='text-sm text-gray-500 mt-2 ml-1'>Responses from you surveys</p>
       </div>
       <ParticipantViewResponseSearch />
-
     </div>
   )
 }
