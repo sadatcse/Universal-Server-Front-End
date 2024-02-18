96dfc5e798b1b08344bfb2b5e2441ebf9e2a1c4f
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-18 09:40:01 +0600

    added terms and condition page

---
+++ src/app/primary/(primary)/terms/page.jsx
@@ -0,0 +1,9 @@
+const Terms = () => {
+    return (
+        <div className="w-[80%] mx-auto  my-4 mb-7">
+            <p>This is terms and condition page</p>
+        </div>
+    );
+};
+
+export default Terms;
