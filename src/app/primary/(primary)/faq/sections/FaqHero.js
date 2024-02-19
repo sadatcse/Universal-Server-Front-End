8acb52cf5a6c699753384cbc5f0e680e09438c39
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-19 23:02:49 +0600

    redesigned FAQ whole page

--- src/app/primary/(primary)/faq/sections/FaqHero.js
+++ src/app/primary/(primary)/faq/sections/FaqHero.js
@@ -1,13 +1,10 @@
 
 function FaqHero({faqData}) {
     return (
-        <section className=" relative z-[1] dark:bg-transparent bg-amber-200" id="faqHero">
-            <div className="w-full h-full absolute top-0 left-0 z-[-1] opacity-20" style={{ backgroundImage: "url('https://i.pinimg.com/564x/e7/38/8b/e7388be6e75e602eb3dc5fef7a5dec71.jpg')" }} >
-
-            </div>
+        <section className=" relative z-[1] dark:bg-transparent bg-[#E9F1FA]" id="faqHero">
             <div className="container grid grid-cols-1 md:grid-cols-2 p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between gap-4 gap-x-10">
                 {faqData.map((faqItem, index) => (
-                    <a href={`#ans${index === 0 ? "" : index }`} key={index} className="collapse collapse-plus bg-white hover:bg-neutral-800 hover:text-white scale-100 hover:scale-105 transition-all duration-700 text-neutral-800">
+                    <a href={`#ans${index === 0 ? "" : index }`} key={index} className="collapse collapse-plus bg-white hover:bg-[#00ABE4] hover:text-white scale-100 hover:scale-105 transition-all duration-300 text-neutral-800">
                         <div className="collapse-title text-xl font-medium">
                             {faqItem.question}
                         </div>
