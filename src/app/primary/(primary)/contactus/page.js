ce3392a9b620fb976923a26ca9d5a2d1900efc51
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-19 19:15:49 +0600

    fixed contactus page color

--- src/app/primary/(primary)/contactus/page.js
+++ src/app/primary/(primary)/contactus/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ContactUS = () => {
     return (
         <section className="hero container mx-auto my-16 relative overflow-hidden px-0 lg:px-24">
-                <div className="p-8 flex-shrink-0 w-full  bg-blue-200 rounded-lg relative overflow-hidden text-neutral-800 z-[1]">
+                <div className="p-8 flex-shrink-0 w-full  bg-[#E9F1FA] rounded-lg relative overflow-hidden text-neutral-800 z-[1]">
                     <Image width={400} height={400} src="https://i.pinimg.com/564x/4d/3a/1a/4d3a1a49fb878b60a07f4d34c06c9b1b.jpg" alt="" className='w-[300px] md:w-[500px] absolute bottom-0 -right-32 mix-blend-multiply opacity-25 rotateY z-[-1]' />
                     <h2 className='text-4xl font-bold mb-6'>Reach Us</h2>
                     <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -32,7 +32,7 @@ const ContactUS = () => {
                             <textarea name="" className='w-full min-h-[150px] input input-bordered dark:text-white' placeholder="Message"></textarea>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-neutral">Send</button>
+                            <button className="border border-transparent btn rounded-md bg-[#00ABE4] text-white shadow font-semibold  hover:bg-white hover:border-[#00abe4]  hover:text-[#00ABE4] transition duration-500 ease-in-out">Send</button>
                         </div>
                     </form>
                 </div>
