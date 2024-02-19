129d8d6029fc691f1220da0cb24797b3f3cbe8c3
Minhajul Islam <mmminhaj221@gmail.com>
2024-02-19 19:04:20 +0600

    fixed colors in meet our team section on about us page

--- src/components/primary/aboutComponents/Team.js
+++ src/components/primary/aboutComponents/Team.js
@@ -25,11 +25,11 @@ const Team = () => {
     },[currentTeam])
     return (
         <div className='container mx-auto grid grid-cols-6 mb-12'>
-        <h1 className='text-4xl font-bold py-4 col-span-full dark:text-white'>Meet the team</h1>
+        <h1 className='text-4xl font-semibold text-center mb-3 py-4 col-span-full dark:text-white'>Meet the team</h1>
             <div className="col-span-full md:col-span-4">
             
                 <div className="w-full text-lg relative z-[1] group overflow-hidden p-8 hidden md:block text-neutral-800">
-                    <div className="h-0 group-hover:h-full w-full duration-200 transition-all absolute top-0 left-0 rounded-full scale-150 bg-blue-200 z-[-1] text-lg showBg"></div>
+                    <div className="h-0 group-hover:h-full w-full duration-200 transition-all absolute top-0 left-0 rounded-full scale-150 bg-[#E9F1FA] z-[-1] text-lg showBg"></div>
                     <p><b className='text-xl'>Name: </b> {teamData[currentTeam]?.name}</p>
                     <p><b className='text-xl'>Title: </b> {teamData[currentTeam]?.title}</p>
                     <p><b className='text-xl'>Description: </b> {teamData[currentTeam]?.description} </p>
@@ -40,7 +40,7 @@ const Team = () => {
                         <a href="#" className="w-8 h-8 flex justify-center items-center rounded-full bg-white"><FaFacebook /></a>
                     </div>
                 </div>
-                <div className="w-full p-4 grid grid-cols-1 sm:grid-cols-2 justify-center lg:grid-cols-4 md:max-h-[400px] md:overflow-auto gap-6 bg-amber-100">
+                <div className="w-full p-4 grid grid-cols-1 sm:grid-cols-2 justify-center lg:grid-cols-4 md:max-h-[400px] md:overflow-auto gap-6 bg-white border-b">
                 {teamData && teamData.map((member, idx)=> (
                     <TeamCard key={idx} position={idx} member={member} setCurrentTeam={setCurrentTeam} currentTeam={currentTeam} />
                 ))}
