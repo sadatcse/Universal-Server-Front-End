97a18cc4a6779bf54ad3fb2aa46f9b56479ef4da
Anam Aadel <rruholislam1998@gmail.com>
2024-02-22 13:00:59 +0600

    designed dashboard sidebar

--- src/components/shared/GroupLink.js
+++ src/components/shared/GroupLink.js
@@ -5,10 +5,10 @@ import PlusOrMinus from './PlusOrMinus';
 function GroupLink({ children, groupName, ...rest }) {
   const [openSubRoute, setOpenSubRoute] = useState(false);
   return (
-    <li className={`flex items-center text-md hover:text-neutral-800  hover:bg-blue-100 ${openSubRoute ? "bg-blue-100 text-neutral-800" : "text-white"} relative`} >
-      <div className={`w-full rounded-none hover:text-neutral-800 ${openSubRoute ? "bg-amber-200 text-neutral-800 hover:bg-amber-200" : "text-white"}`} onClick={() => setOpenSubRoute(!openSubRoute)}>{groupName}</div>
+    <li className={`flex items-center text-md hover:text-neutral-800  hover:bg-white ${openSubRoute ? "bg-white text-neutral-800" : "text-white"} relative`} >
+      <div className={`w-full rounded-none hover:text-neutral-800 ${openSubRoute ? "bg-stone-300 text-neutral-800 hover:bg-stone-300" : "text-white"}`} onClick={() => setOpenSubRoute(!openSubRoute)}>{groupName}</div>
       <PlusOrMinus isTrue={openSubRoute} />
-      <ul className={`flex flex-col gap-2 before:top-0 transition-all duration-500 w-full overflow-auto ${openSubRoute ? "h-32 mt-2" : "h-[0] p-0 mt-0"}`}>
+      <ul className={`flex flex-col gap-2 before:top-0 transition-all duration-500 w-full overflow-auto ${openSubRoute ? "h-40 mt-2" : "h-[0] p-0 mt-0"}`}>
         {children}
 
       </ul>
--- src/components/survey_creator/export_response_survey/components/ExportResponseCard.js
+++ src/components/survey_creator/export_response_survey/components/ExportResponseCard.js
@@ -2,13 +2,13 @@ import { FaFileExport } from 'react-icons/fa'
 
 function ExportResponseCard({setOpenModal, data}) {
     return (
-        <div className="flex flex-col group items-start text-left p-6 border shadow-lg rounded-md bg-blue-300 hover:bg-blue-200 hover:shadow-2xl hover:scale-110 transition-all duration-300 dark:text-white dark:hover:text-neutral relative overflow-hidden before:w-[90%] before:h-[90%] before:border-4 before:border-white before:rounded-md before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2">
-        <div className="absolute top-0 -left-full w-full h-full bg-red-300 flex justify-center items-center gap-3 text-neutral-800 text-3xl group-hover:left-0 transition-all duration-300 ">
-        <button className="hover:text-white p-3 border border-white rounded-lg" onClick={()=> setOpenModal(true)}><FaFileExport /></button>
+        <div className="flex flex-col group items-start text-left p-6 border-2 border-stone-400 shadow-lg rounded-md  hover:shadow-2xl hover:scale-110 transition-all duration-300 dark:text-white dark:hover:text-neutral relative overflow-hidden pr-12">
+        <div className="absolute top-0 right-0 w-10 h-full flex justify-start items-center gap-3 text-neutral-800 text-md transition-all duration-300 flex-col p-2">
+        <button className="btn btn-outline btn-sm" onClick={()=> setOpenModal(true)}><FaFileExport /></button>
         
          </div>
             
-         <h3 className="my-3 text-3xl font-semibold">{data.SurveyTitle}</h3>
+         <h3 className="my-3 text-xl font-semibold">{data.SurveyTitle}</h3>
                 <p><b>Q: </b> {data.Questions.length}</p>
             <div >
             </div>
